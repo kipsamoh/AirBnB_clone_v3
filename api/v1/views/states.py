a9562f7d252fecbbb3088439d568efc1eb57ec3b
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2024-02-03 09:49:20 +0300

    states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" View for State objects that handles default API actions """
+""" View for _State objects that handles default API actions """
 from api.v1.views import app_views
 from flask import jsonify, abort, make_response, request
 from models import storage
@@ -8,14 +8,14 @@ from models.state import State
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def states():
-    """ Retrieves the list of all State objects """
+    """ _Retrieves the list of all State objects """
     d_states = storage.all(State)
     return jsonify([obj.to_dict() for obj in d_states.values()])
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def r_state_id(state_id):
-    """ Retrieves a State object """
+    """ _Retrieves a State object """
     state = storage.get("State", state_id)
     if not state:
         abort(404)
@@ -25,7 +25,7 @@ def r_state_id(state_id):
 @app_views.route('/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
 def del_state(state_id):
-    """ Deletes a State object """
+    """ _Deletes a State object """
     state = storage.get("State", state_id)
     if not state:
         abort(404)
@@ -36,7 +36,7 @@ def del_state(state_id):
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def post_state():
-    """ Creates a State object """
+    """ _Creates a State object """
     new_state = request.get_json()
     if not new_state:
         abort(400, "Not a JSON")
@@ -50,7 +50,7 @@ def post_state():
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def put_state(state_id):
-    """ Updates a State object """
+    """ _Updates a State object """
     state = storage.get("State", state_id)
     if not state:
         abort(404)
