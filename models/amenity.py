0695aabbbf4abd1b368cb4bfa36ba2af3162be1a
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2024-02-03 14:43:00 +0300

    amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" holds class Amenity"""
+""" _holds class Amenity"""
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -9,7 +9,7 @@ from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Representation of Amenity """
+    """_Representation of Amenity """
     if models.storage_t == 'db':
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
@@ -17,5 +17,5 @@ class Amenity(BaseModel, Base):
         name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
+        """_initializes Amenity"""
         super().__init__(*args, **kwargs)
